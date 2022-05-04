753157712aed1ae76cbb32649e6f25d666a7a13a
Furkan-Erg <furkanerguldurenler@hotmail.com>
2022-05-04 15:49:48 +0300

    client service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ETicaretClient';
 }
 
+$.get('https://localhost:7187/api/products', (data) => {
+  console.log(data);
+});
